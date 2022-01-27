a803076f3580f11e2f74c5a28ea9ec8d54492484
mjpurin <mjpurin@gmail.com>
2022-01-27 14:06:40 +0900

    illumiball完成

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class Hole : MonoBehaviour
 {
     public string targetTag;
+    bool isHolding;
 
-    private void OnTriggerStay(Collider other) {
+    public bool IsHolding() {
+        return isHolding;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == targetTag) {
+            isHolding = true;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == targetTag) {
+            isHolding = false;
+        }
+    }
+    void OnTriggerStay(Collider other) {
         Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
 
         Vector3 direction = other.gameObject.transform.position - transform.position;
@@ -14,7 +29,7 @@ public class Hole : MonoBehaviour
 
         if(other.gameObject.tag == targetTag) {
             r.velocity *= 0.9f;
-            r.AddForce(direction * -20.0f, ForceMode.Acceleration);
+            r.AddForce(-direction * 20.0f, ForceMode.Acceleration);
         } else {
             r.AddForce(direction * 80.0f, ForceMode.Acceleration);
         }
